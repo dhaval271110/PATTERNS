8371615858f6fc4cefc1406f9be90068c8e0429a
dhaval271110 <dhavalsiddhapura23@gmail.com>
2025-11-20 11:17:24 +0530

    Diamond pattern

---
+++ numberdiamond.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void main() {
+    int n = 4;
+
+    for (int i = 1; i <= n; i++) {
+        int x = 1;
+
+        for (int k = 0; k < n - i; k++) {
+            printf("  ");
+        }
+
+        for (int j = 1; j <= (2 * i) - 1; j++) {
+            printf("%d ", x);
+            if (j < i)
+                x++;
+            else
+                x--;
+        }
+        printf("\n");
+    }
+
+    for (int i = n - 1; i >= 1; i--) {
+        int x = 1;
+
+        for (int k = 0; k < n - i; k++) {
+            printf("  ");
+        }
+
+        for (int j = 1; j <= (2 * i) - 1; j++) {
+            printf("%d ", x);
+            if (j < i)
+                x++;
+            else
+                x--;
+        }
+        printf("\n");
+    }
+}
